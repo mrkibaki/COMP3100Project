bacb7222d98bbf165c62dbebb3348e1c59889b95
kibakibaki <mrkibaki@gmail.com>
2022-05-06 14:57:40 +1000

    modification of w8 workshop

--- W8_Workshop/MyClient.java
+++ W8_Workshop/MyClient.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 
 public class MyClient {
-    public Socket;  
+    public Socket s;
     DataOutputStream dout;
     BufferedReader din;
 
@@ -75,11 +75,13 @@ public class MyClient {
             String firstJob;
             firstJob = client.sender("REDY");
 
+            String fJ[];
+            fJ = firstJob.split(" ");
             /******************** Getting server list ************************/
             // gets all servers from cluster side and stored in "String list"
 
-            // store server number in "serverNum" by function "serverNum"
-            String serverNumber = client.sender("GETS All");
+            // store the number of servers in "serverNum" by function "serverNum"
+            String serverNumber = client.sender("GETS Capable " + fJ[4] + " " + fJ[5] + " " + fJ[6]);
             int serverNum = client.serverNum(serverNumber);
 
             // store server info in String "list" by fucntion "serverInfo"
@@ -87,64 +89,76 @@ public class MyClient {
             list = client.serverInfo("OK", serverNum);
             client.sender("OK");
 
+            // handling the server info split into indexes
+            String firstServerList[] = list.split("\n");
+            String firstServerListFirstServer[] = firstServerList[0].split(" ");
+            client.sender("SCHD " + "0 " + firstServerListFirstServer[0] + " " + firstServerListFirstServer[1]);
+
             /********************** Server List Array ***********************/
             // create a string arraylist (called "serverList")
             // to store all server info needed in
-            List<String[]> serverList = new ArrayList<String[]>();// servers
-            String[] listLine = list.split("\n");
-            System.out.println(listLine.length);
+            // List<String[]> serverList = new ArrayList<String[]>();// servers
+            // String[] listLine = list.split("\n");
+            // System.out.println(listLine.length);
 
             // split server info by how many \n among them
-            String serverInfo[];
-            for (int i = 0; i < listLine.length; i++) {
-                // split indexes by how many spaces among them
-                serverInfo = new String[3];
-                String index[] = listLine[i].split(" ");
-                serverInfo[0] = index[0];// server type
-                serverInfo[1] = index[1];// server ID
-                serverInfo[2] = index[4];// server cores
-                serverList.add(serverInfo);// serverList CONTAINS serverInfo[]
-            }
+            // String serverInfo[];
+            // for (int i = 0; i < listLine.length; i++) {
+            // // split indexes by how many spaces among them
+            // serverInfo = new String[3];
+            // String index[] = listLine[i].split(" ");
+            // serverInfo[0] = index[0];// server type
+            // serverInfo[1] = index[1];// server ID
+            // serverInfo[2] = index[4];// server cores
+            // serverList.add(serverInfo);// serverList CONTAINS serverInfo[]
+            // }
 
             /************************** LRR **********************************/
             // finding the largest servers
-            String[] largest = null;
-            for (String[] node : serverList) {
-                if (largest == null || Integer.parseInt(node[2]) > Integer.parseInt(largest[2])) {
-                    largest = node;
-                }
-            }
+            // String[] largest = null;
+            // for (String[] node : serverList) {
+            // if (largest == null || Integer.parseInt(node[2]) >
+            // Integer.parseInt(largest[2])) {
+            // largest = node;
+            // }
+            // }
 
             // collect all servers with largest cores in the list, store them in
             // "utilisedServers"
-            List<String[]> utilisedServers = new ArrayList<String[]>();
-            for (String[] node : serverList) {
-                if (Integer.parseInt(node[2]) == Integer.parseInt(largest[2])) {
-                    utilisedServers.add(node);
-                }
-            }
+            // List<String[]> utilisedServers = new ArrayList<String[]>();
+            // for (String[] node : serverList) {
+            // if (Integer.parseInt(node[2]) == Integer.parseInt(largest[2])) {
+            // utilisedServers.add(node);
+            // }
+            // }
 
             // Iterator for utilisedServers list
-            Iterator<String[]> iter = utilisedServers.iterator();
+            // Iterator<String[]> iter = utilisedServers.iterator();
             // Iterator<String[]> beginner = utilisedServers.iterator();
 
             // asign the firstJob
-            String splitFirstJob[] = firstJob.split(" ");
-            client.sender(
-                    "SCHD " + splitFirstJob[2] + " " + utilisedServers.get(0)[0] + " " + utilisedServers.get(0)[1]);
-            System.out
-                    .println("JOB ID " + splitFirstJob[2] + " has been scheduled to " + utilisedServers.get(0)[0] + " "
-                            + utilisedServers.get(0)[1] + "\n");
+            // String splitFirstJob[] = firstJob.split(" ");
+            // client.sender(
+            // "SCHD " + splitFirstJob[2] + " " + utilisedServers.get(0)[0] + " " +
+            // utilisedServers.get(0)[1]);
+            // System.out
+            // .println("JOB ID " + splitFirstJob[2] + " has been scheduled to " +
+            // utilisedServers.get(0)[0] + " "
+            // + utilisedServers.get(0)[1] + "\n");
 
             // assign jobs to utilisedServers
             String getJob = "";
             String jobInfo[];
             String box[] = new String[3];
-            if (iter.hasNext()) {
-                box = iter.next();
-                // Because the firstJob has been processed, first loop shuld start from the
-                // second server
-            }
+            // String serverInfo;
+            String server;
+            String serverList[];
+            String firstServerInfo[] = new String[3];
+            // if (iter.hasNext()) {
+            // box = iter.next();
+            // // Because the firstJob has been processed, first loop shuld start from the
+            // // second server
+            // }
             while (true) {
                 getJob = client.sender("REDY");
                 if (getJob.equals("NONE")) {
@@ -152,16 +166,41 @@ public class MyClient {
                 }
                 jobInfo = getJob.split(" ");
                 if (!jobInfo[0].equals("JCPL")) {
-                    if (iter.hasNext()) {
-                        box = iter.next();
-                    }
-                    client.sender("SCHD " + jobInfo[2] + " " + box[0] + " " + box[1]);
-                    System.out
-                            .println("JOB ID " + jobInfo[2] + " has been scheduled to " + box[0] + " " + box[1] + "\n");
+                    // if (iter.hasNext()) {
+                    // box = iter.next();
+                    // }
+                    // serverInfo =
 
-                    if (!iter.hasNext()) {
-                        iter = utilisedServers.iterator();
+                    // store server info in String "list" by fucntion "serverInfo"
+
+                    String num;
+                    num = client.sender("GETS Capable " + jobInfo[4] + " " + jobInfo[5] + " " + jobInfo[6]);
+                    int sNum = client.serverNum(num);
+
+                    for (int i = 0; i < jobInfo.length; i++) {
+                        System.out.println("This is the start of the job Info" + jobInfo[i] + " end");
                     }
+
+                    server = client.serverInfo("OK", sNum);
+                    client.sender("OK");
+
+                    serverList = server.split("\n");
+                    for (int i = 0; i < serverList.length; i++) {
+                        System.out.println("This is the server list " + serverList[i] + "end of the server list");
+                    }
+                    firstServerInfo = serverList[0].split(" ");
+                    box[0] = firstServerInfo[0];
+                    box[1] = firstServerInfo[1];
+
+                    client.sender("SCHD " + jobInfo[2] + " " + box[0] + " " + box[1]);
+
+                    // System.out
+                    // .println("JOB ID " + jobInfo[2] + " has been scheduled to " + box[0] + " " +
+                    // box[1] + "\n");
+
+                    // if (!iter.hasNext()) {
+                    // iter = utilisedServers.iterator();
+                    // }
                 }
             }
 
